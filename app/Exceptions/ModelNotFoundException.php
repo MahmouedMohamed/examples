2a477e9871ea823226e3fc4f9a16e716c24e4cc7
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-07-30 16:33:44 +0300

    Create Model not found exception

---
+++ app/Exceptions/ModelNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Traits\ApiResponse;
+use Exception;
+
+class ModelNotFoundException extends Exception
+{
+    use ApiResponse;
+
+    public function __construct()
+    {
+    }
+
+    public function render()
+    {
+        return $this->error(__('model_not_found'), 404);
+    }
+}
