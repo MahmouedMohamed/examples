c73f88113eb537ec87c74835aee44947eb94f89f
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-07-30 04:43:47 +0300

    Add Book Observer and it's operations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\BookObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use MongoDB\Laravel\Eloquent\Model;
 
+#[ObservedBy([BookObserver::class])]
 class Book extends Model
 {
     use HasFactory, SoftDeletes;
@@ -19,4 +22,10 @@ class Book extends Model
         'publication_date',
         'author',
     ];
+
+    const VIEW_TOPIC = 'book-viewed';
+
+    const CREATE_TOPIC = 'book-created';
+
+    const UPDATE_TOPIC = 'book-updated';
 }
---
+++ app/Observers/BookObserver.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Observers;
+
+use App\Interfaces\KafkaServiceInterface;
+use App\Models\Book;
+
+class BookObserver
+{
+    public function __construct(private KafkaServiceInterface $kafkaService) {}
+
+    /**
+     * Handle the Book "viewed" event.
+     */
+    public function viewed(Book $book): void
+    {
+        $this->kafkaService->createMessage(Book::VIEW_TOPIC, [], $book);
+    }
+
+    /**
+     * Handle the Book "created" event.
+     */
+    public function created(Book $book): void
+    {
+        $this->kafkaService->createMessage(Book::CREATE_TOPIC, [], $book);
+    }
+
+    /**
+     * Handle the Book "updated" event.
+     */
+    public function updated(Book $book): void
+    {
+        $this->kafkaService->createMessage(Book::UPDATE_TOPIC, [], $book);
+    }
+
+    /**
+     * Handle the Book "deleted" event.
+     */
+    public function deleted(Book $book): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "restored" event.
+     */
+    public function restored(Book $book): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Book "force deleted" event.
+     */
+    public function forceDeleted(Book $book): void
+    {
+        //
+    }
+}
