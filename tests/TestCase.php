9053d57c289594a5444cfb7d5b4ed3c5524f24ed
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-07-30 19:07:58 +0300

    Add .env.testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+{
+    parent::setUp();
+
+    if (app()->environment('testing')) {
+        config()->set('database.connections.mongodb', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
 }
